eb71f82a41170a761a6a9622463f81805d115a61
mich-man <88162731+surenkov1985@users.noreply.github.com>
2022-09-20 16:50:42 +0300

    разворачивание сервера node.js express.js поключение к бд mySQL

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const router = require("./routes/short.routes");
+const cors = require("cors");
+const path = require("path");
+
+const corsOptions = {
+	origin: "*",
+	credentials: true,
+	optionSuccessStatus: 200,
+};
+
+const router = require("./routes/short.routes");
+const app = express();
+const port = 5000;
+
+app.use(express.json({ extended: true }));
+
+if (process.env.NODE_ENV === "production") {
+	app.use("/", express.static(path.join(__dirname, "client", "dist")));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
+	});
+}
+
+app.use(cors(corsOptions));
+
+app.use("/", router);
+
+app.listen(port, () => {
+	console.log("Hello");
+});
